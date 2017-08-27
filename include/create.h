51c9c1e7de2ee784deab5a2a7ad25ea5fe86ae9e
Sander Mertens <sander.mertens8@gmail.com>
2017-08-27 10:01:30 -0700

    Initial commit

---
+++ include/create.h
@@ -0,0 +1,18 @@
+
+#ifndef CREATE_H
+#define CREATE_H
+
+/* Add include files here */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Insert definitions here */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CREATE_H */
+
